b8b242101b9b5204d14d8fb21f14e70d77fea150
archbaer <pereirakebaili@gmail.com>
2025-03-19 23:16:06 +0000

    minor changes

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -5,8 +5,10 @@ const store = useCounterStore()
 </script>
 
 <template>
-  <h1>Hello</h1>
+  <div>
+    <h1>Hello</h1>
 
-  {{ store.count }}
-  Double: {{ store.doubleCount }}
+    {{ store.count }}
+    Double: {{ store.doubleCount }}
+  </div>
 </template>
